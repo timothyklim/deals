2db457452624bca13da04e6f0e74e9148e350208
Timothy Klim <klimtimothy@gmail.com>
2011-10-20 19:32:56 +0400

    fix groupon cities

--- mate.rb
+++ mate.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH << File.expand_path('../', __FILE__)
 require 'config/environment'
 
+# p http.response_header.status
+# p http.response_header
+
 # For short calling
 def EM::HR *args
   EM::HttpRequest.new *args
@@ -13,32 +16,24 @@ EM.run do
     response = Nokogiri::HTML cities.response
     links = response / 'div.cities a'
 
-    links.each do |link|
-      puts URI.join('http://groupon.ru/', link['href']).to_s
-      city = EM::HR(URI.join('http://groupon.ru/', link['href']).to_s).get redirects: 1
-      city.callback do
-        puts city.response
+    links.each_with_index do |link, index|
+      link = URI.join('http://groupon.ru/', link['href']).to_s
+      puts link
+      http = EM::HR(link).get redirects: 1
+      http.callback do
+        response = Nokogiri::HTML http.response
+        offers = response / 'div.offer[role=offer]'
+        offers.each do |offer|
+          puts (offer / 'h2').first.content.strip
+          puts
+        end
+      end
+      
+      http.errback do
+        puts 'Error!'
       end
     end
   end
-
-  # http = EM::HR('http://groupon.ru/moscow').get
-  # http.callback do
-  #   # p http.response_header.status
-  #   # p http.response_header
-  #   response = Nokogiri::HTML http.response
-  #   offers = response / 'div.offer[role=offer]'
-  #   offers.each do |offer|
-  #     puts (offer / 'h2').first.content.strip
-  #     puts
-  #   end
-  # 
-  #   EM.stop
-  # end
-  # 
-  # http.errback do
-  #   puts 'Error!'
-  # end
 end
 
 puts 'Stop EM'
